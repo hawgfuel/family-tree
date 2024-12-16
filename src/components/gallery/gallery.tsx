e723f505506c1428f1bfde01bacd076f9ab205e7
Alan Werstler <hawgfuel@gmail.com>
2024-12-16 11:47:10 -0800

    adding paging in image modal overlay

--- src/components/gallery/gallery.tsx
+++ src/components/gallery/gallery.tsx
@@ -8,6 +8,7 @@ interface ImageFile {
 
 export function Gallery() {
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
   const [shuffledImageFileNames, setShuffledImageFileNames] = useState<ImageFile[]>([]);
 
   const getImageUrl = (photo: string) => {
@@ -15,18 +16,37 @@ export function Gallery() {
     return `${path}${photo}`;
   };
 
-  const handleImageClick = (image: string) => {
+  const handleImageClick = (image: string, index: number) => {
+    setSelectedIndex(index);
     setSelectedImage(image);
   };
 
   const handleCloseImage = () => {
+    setSelectedIndex(null);
     setSelectedImage(null);
+  };
+
+  const handleNextImage = () => {
+    if (selectedIndex !== null) {
+      const nextIndex = (selectedIndex + 1) % shuffledImageFileNames.length;
+      setSelectedImage(shuffledImageFileNames[nextIndex].value);
+      setSelectedIndex(nextIndex);
+    }
+  };
+
+  const handlePrevImage = () => {
+    if (selectedIndex !== null) {
+      const prevIndex =
+        (selectedIndex - 1 + shuffledImageFileNames.length) % shuffledImageFileNames.length;
+      setSelectedImage(shuffledImageFileNames[prevIndex].value);
+      setSelectedIndex(prevIndex);
+    }
   };
 
   const shuffleArray = (array: any[]) => {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+      [array[i], array[j]] = [array[j], array[i]]; 
     }
     return array;
   };
@@ -35,7 +55,7 @@ export function Gallery() {
   useEffect(() => {
     const shuffled = shuffleArray([...imageFileNames]);
     setShuffledImageFileNames(shuffled);
-  }, []); // Empty dependency array ensures this runs only once on mount
+  }, []);
 
   return (
     <>
@@ -43,23 +63,36 @@ export function Gallery() {
         Images are randomly shuffled each time the gallery is visited. Click on an image to enlarge it. Click on the enlarged image to close.
       </p>
       <div className="fade-in gallery-container">
-        {shuffledImageFileNames.map(({ value }: ImageFile) => (
+        {shuffledImageFileNames.map(({ value }, index) => (
           <div
             className="gallery-image image-size-w"
             key={value}
-            onClick={() => handleImageClick(value)}
+            onClick={() => handleImageClick(value, index)}
           >
             <img alt={value} src={getImageUrl(value)} />
           </div>
         ))}
 
         {selectedImage && (
-          <div className="enlarged-image-overlay fade-in" onClick={handleCloseImage}>
+          <div className="enlarged-image-overlay fade-in">
+            <button className="prev-button" onClick={handlePrevImage} role='previous image button'>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid meet" width="100%" height="100%">
+                    <circle cx="50" cy="50" r="45" fill="white" stroke-width="2" />
+                    <polygon points="55,35 35,50 55,65" fill="black" />
+                </svg>
+            </button>
             <img
-              src={getImageUrl(selectedImage)}
+              src={getImageUrl(selectedImage)} 
+              onClick={handleCloseImage}
               alt="Enlarged"
               className="enlarged-image"
             />
+            <button className="next-button" onClick={handleNextImage} role='next image button'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid meet" width="100%" height="100%">
+                <circle cx="50" cy="50" r="45" fill="white" stroke-width="2" />
+                <polygon points="45,35 65,50 45,65" fill="black" />
+            </svg>
+            </button>
           </div>
         )}
       </div>
