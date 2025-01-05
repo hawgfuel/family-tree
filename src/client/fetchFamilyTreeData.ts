b5a9e9b3b4287badd8417d4d22d7c55b473122d1
Alan Werstler <hawgfuel@gmail.com>
2025-01-05 10:56:45 -0800

    removed unused code

--- src/client/fetchFamilyTreeData.ts
+++ src/client/fetchFamilyTreeData.ts
@@ -2,7 +2,6 @@ import { FamilyMember } from '../common/types';
 import { formatFamilyMemberData } from '../utilities/formatData';
 
 export const fetchFamilyTreeData = async (): Promise<FamilyMember[]> => {
-  const apiKey = import.meta.env.VITE_COCKPIT_API_KEY;
   const response = await fetch(`https://www.guicoder.com/familytree/api/content/items/FamilyTree?populate=1`);
   const data = await response.json();
   const formattedData = formatFamilyMemberData(data);
